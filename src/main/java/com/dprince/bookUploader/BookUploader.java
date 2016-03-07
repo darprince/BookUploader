8da1676c1b92aee7b8c1a46cf9421f8098cc3930
Darren <dprince@tutelatechnologies.com>
2016-03-07 10:23:01 -0800

    Added check to see if book is already in db.

--- src/main/java/com/dprince/bookUploader/BookUploader.java
+++ src/main/java/com/dprince/bookUploader/BookUploader.java
@@ -12,129 +12,131 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BookUploader {
-	static Logger log = LogManager.getLogger(BookUploader.class.getName());
+    static Logger log = LogManager.getLogger(BookUploader.class.getName());
 
-	private static final String SERVER = "www.princewalker.com";
-	private static final int PORT = 21;
-	private static final String USER = "prin9762";
-	private static final String PASS = "W@lker75";
+    private static final String SERVER = "www.princewalker.com";
+    private static final int PORT = 21;
+    private static final String USER = "prin9762";
+    private static final String PASS = "W@lker75";
 
-	public static void main(String[] args) {
-		final Book book = setUpInstanceOfBook(args[0]);
+    public static void main(String[] args) {
+        final Book book = setUpInstanceOfBook(args[0]);
 
-		boolean success = false;
-		try {
-			success = checkFileExists("books/" + book.filename);
-			if (success) {
-				log.info("Book exists.");
-				System.exit(0);
-			}
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
+        boolean success = false;
+        try {
+            success = checkFileExists("books/" + book.filename);
+            if (success) {
+                log.info("Book exists.");
+                System.exit(0);
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
 
-		log.info("getIsbnFromGoogle()");
-		final List<String> potentialIsbnList = Google.getPotentialIsbnsFromGoogle(book.author, book.title);
+        log.info("getIsbnFromGoogle()");
+        final List<String> potentialIsbnList = Google.getPotentialIsbnsFromGoogle(book.author,
+                book.title);
+        DataBase.insertFromUpload(potentialIsbnList, book);
 
-		if (DataBase.insertFromUpload(potentialIsbnList, book)) {
-			uploadFile(book.filename, book.fullFilename, book.isbn);
-		}
-	}
+        if (!DataBase.checkIfBookExistsInDB(book.isbn)) {
+            uploadFile(book.filename, book.fullFilename, book.isbn);
+        }
+    }
 
-	/**
-	 * @param arg
-	 *            User input of filename to be uploaded and database populated
-	 *            with.
-	 * @return Book with author, title, filename and fullFilename. {@link Book}
-	 */
-	public static Book setUpInstanceOfBook(String arg) {
-		final Book book = Book.getInstance();
+    /**
+     * @param arg
+     *            User input of filename to be uploaded and database populated
+     *            with.
+     * @return Book with author, title, filename and fullFilename. {@link Book}
+     */
+    public static Book setUpInstanceOfBook(String arg) {
+        final Book book = Book.getInstance();
 
-		final String filename = arg.substring(arg.lastIndexOf("\\") + 1, arg.length());
-		log.info(arg);
-		final String[] bits = filename.split(" - ");
-		final String author = bits[0];
-		final String title = bits[1].substring(0, bits[1].lastIndexOf("."));
+        final String filename = arg.substring(arg.lastIndexOf("\\") + 1, arg.length());
+        log.info(arg);
+        final String[] bits = filename.split(" - ");
+        final String author = bits[0];
+        final String title = bits[1].substring(0, bits[1].lastIndexOf("."));
 
-		book.setAuthor(author);
-		book.setTitle(title);
-		book.setFilename(filename);
-		book.setFullFilename(arg);
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setFilename(filename);
+        book.setFullFilename(arg);
 
-		log.info("Setting up book...");
-		log.info("Filename: " + filename);
-		log.info("Author: " + author + "\nTitle: " + title);
-		return book;
-	}
+        log.info("Setting up book...");
+        log.info("Filename: " + filename);
+        log.info("Author: " + author + "\nTitle: " + title);
+        return book;
+    }
 
-	/**
-	 * @param filename
-	 * @param fullFileName
-	 *            filename with path.
-	 * @return true if upload succeeds, false otherwise.
-	 */
-	public static boolean uploadFile(String filename, String fullFileName, String isbn) {
-		log.info("uploadFile()");
-		final FTPClient ftpClient = new FTPClient();
-		try {
-			ftpClient.connect(SERVER, PORT);
-			ftpClient.login(USER, PASS);
-			ftpClient.enterLocalPassiveMode();
+    /**
+     * @param filename
+     * @param fullFileName
+     *            filename with path.
+     * @return true if upload succeeds, false otherwise.
+     */
+    public static boolean uploadFile(String filename, String fullFileName, String isbn) {
+        log.info("uploadFile()");
+        final FTPClient ftpClient = new FTPClient();
+        try {
+            ftpClient.connect(SERVER, PORT);
+            ftpClient.login(USER, PASS);
+            ftpClient.enterLocalPassiveMode();
 
-			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
 
-			final File firstLocalFile = new File(fullFileName);
+            final File firstLocalFile = new File(fullFileName);
 
-			final String firstRemoteFile = "books/" + filename;
+            final String firstRemoteFile = "books/" + filename;
 
-			final InputStream inputStream = new FileInputStream(firstLocalFile);
+            final InputStream inputStream = new FileInputStream(firstLocalFile);
 
-			log.info("Start uploading first file, isbn: " + isbn);
-			final boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
-			inputStream.close();
-			if (done) {
-				log.info("The first file is uploaded successfully.");
-			}
-		} catch (final IOException ex) {
-			System.out.println("Error: " + ex.getMessage());
-			ex.printStackTrace();
-			return false;
-		} finally {
-			try {
-				if (ftpClient.isConnected()) {
-					ftpClient.logout();
-					ftpClient.disconnect();
-				}
-			} catch (final IOException ex) {
-				ex.printStackTrace();
-			}
-		}
+            log.info("Start uploading first file, isbn: " + isbn);
+            final boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
+            inputStream.close();
+            if (done) {
+                log.info("The first file is uploaded successfully.");
+            }
+        } catch (final IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (final IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
-		boolean success = false;
-		try {
-			success = checkFileExists("books/" + filename);
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
+        boolean success = false;
+        try {
+            success = checkFileExists("books/" + filename);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
 
-		return success;
-	}
+        return success;
+    }
 
-	static boolean checkFileExists(String filepath) throws IOException {
-		final FTPClient ftpClient = new FTPClient();
-		try {
-			ftpClient.connect(SERVER, PORT);
-			ftpClient.login(USER, PASS);
-			ftpClient.enterLocalPassiveMode();
+    static boolean checkFileExists(String filepath) throws IOException {
+        final FTPClient ftpClient = new FTPClient();
+        try {
+            ftpClient.connect(SERVER, PORT);
+            ftpClient.login(USER, PASS);
+            ftpClient.enterLocalPassiveMode();
 
-			final InputStream inputStream = ftpClient.retrieveFileStream(filepath);
-			final Integer returnCode = ftpClient.getReplyCode();
-			if (inputStream == null || returnCode == 550) {
-				return false;
-			}
-		} catch (final Exception e) {
+            final InputStream inputStream = ftpClient.retrieveFileStream(filepath);
+            final Integer returnCode = ftpClient.getReplyCode();
+            if (inputStream == null || returnCode == 550) {
+                return false;
+            }
+        } catch (final Exception e) {
 
-		}
-		return true;
-	}
+        }
+        return true;
+    }
 }
--- src/main/java/com/dprince/bookUploader/DataBase.java
+++ src/main/java/com/dprince/bookUploader/DataBase.java
@@ -15,202 +15,218 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class DataBase {
-	static Logger log = LogManager.getLogger(DataBase.class.getName());
+    static Logger log = LogManager.getLogger(DataBase.class.getName());
 
-	private static Connection connect = null;
-	private static Statement statement = null;
+    private static Connection connect = null;
+    private static Statement statement = null;
 
-	/**
-	 * Takes a list of ISBNs, finds one that goodreads recognizes, populates the
-	 * {@link Book} and inserts into table.
-	 *
-	 * @param isbnList
-	 *            List of isbns returned from google API related to the book
-	 *            being queried
-	 * @param book
-	 *            {@link Book}
-	 * @return True if table insert succeeded, false otherwise.
-	 */
-	public static boolean insertFromUpload(List<String> isbnList, Book book) {
-		connectToDB();
+    /**
+     * Takes a list of ISBNs, finds one that goodreads recognizes, populates the
+     * {@link Book} and inserts into table.
+     *
+     * @param isbnList
+     *            List of isbns returned from google API related to the book
+     *            being queried
+     * @param book
+     *            {@link Book}
+     * @return True if table insert succeeded, false otherwise.
+     */
+    public static boolean insertFromUpload(List<String> isbnList, Book book) {
+        connectToDB();
 
-		log.info("ISBN list size: " + isbnList.size());
-		try {
-			return getBookInfoAndInsert(isbnList, book);
-		} catch (final Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+        log.info("ISBN list size: " + isbnList.size());
+        try {
+            return getBookInfoAndInsert(isbnList, book);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	/**
-	 * Queries the database for needsProccessing = 1, populates book, and
-	 * updates database table.
-	 *
-	 * @return True if Successful, false otherwise.
-	 */
-	public static boolean getFromDBandUpdate() {
-		connectToDB();
+    /**
+     * Queries the database for needsProccessing = 1, populates book, and
+     * updates database table.
+     *
+     * @return True if Successful, false otherwise.
+     */
+    public static boolean getFromDBandUpdate() {
+        connectToDB();
 
-		final List<String> isbnList = getIsbnsFromDBToQuery();
+        final List<String> isbnList = getIsbnsFromDBToQuery();
 
-		printISBNs(isbnList);
+        printISBNs(isbnList);
 
-		return getBookInfoAndUpdate(isbnList);
-	}
+        return getBookInfoAndUpdate(isbnList);
+    }
 
-	/**
-	 * @param isbnList
-	 *            List of isbns returned from google API.
-	 * @param book
-	 *            {@link Book}
-	 */
-	public static boolean getBookInfoAndInsert(List<String> isbnList, Book book) {
-		for (final String bookIsbn : isbnList) {
-			try {
-				final String completeBookInfo = Goodreads.getCompleteBookInfo(bookIsbn);
-				final Book finalBook = populateBook(bookIsbn, completeBookInfo, book);
-				log.info("Book is fully populated: " + finalBook.toString());
-				final boolean inserted = finalBook.insertToTable(connect);
-				if (inserted) {
-					log.info("Inserted ISBN: " + finalBook.isbn);
-					return true;
-				}
-			} catch (final Exception e) {
-				e.printStackTrace();
-				log.info("Error in insert: " + e.getMessage());
-			}
-		}
-		return false;
-	}
+    /**
+     * @param isbnList
+     *            List of isbns returned from google API.
+     * @param book
+     *            {@link Book}
+     */
+    public static boolean getBookInfoAndInsert(List<String> isbnList, Book book) {
+        for (final String bookIsbn : isbnList) {
+            try {
+                final String completeBookInfo = Goodreads.getCompleteBookInfo(bookIsbn);
+                final Book finalBook = populateBook(bookIsbn, completeBookInfo, book);
+                log.info("Book is fully populated: " + finalBook.toString());
+                final boolean inserted = finalBook.insertToTable(connect);
+                if (inserted) {
+                    log.info("Inserted ISBN: " + finalBook.isbn);
+                    return true;
+                }
+            } catch (final Exception e) {
+                e.printStackTrace();
+                log.info("Error in insert: " + e.getMessage());
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * @param isbnList
-	 *            List of isbns returned from google API.
-	 * @return True if successful, false otherwise.
-	 */
-	public static boolean getBookInfoAndUpdate(List<String> isbnList) {
-		for (final String bookIsbn : isbnList) {
-			try {
-				final Book book = Book.getInstance();
-				final String completeBookInfo = Goodreads.getCompleteBookInfo(bookIsbn);
-				final Book finalBook = populateBook(bookIsbn, completeBookInfo, book);
-				finalBook.updateTable(connect);
-			} catch (final Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * @param isbnList
+     *            List of isbns returned from google API.
+     * @return True if successful, false otherwise.
+     */
+    public static boolean getBookInfoAndUpdate(List<String> isbnList) {
+        for (final String bookIsbn : isbnList) {
+            try {
+                final Book book = Book.getInstance();
+                final String completeBookInfo = Goodreads.getCompleteBookInfo(bookIsbn);
+                final Book finalBook = populateBook(bookIsbn, completeBookInfo, book);
+                finalBook.updateTable(connect);
+            } catch (final Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * Takes a {@link JsonObject} in {@link String} form and populates
-	 * {@link Book} object.
-	 *
-	 * @param isbn
-	 * @param completeBookInfo
-	 * @param book
-	 *            Partially populated {@link Book}.
-	 * @return Fully populated {@link Book}.
-	 */
-	private static Book populateBook(String isbn, String completeBookInfo, Book book) {
-		final JsonParser parser = new JsonParser();
-		JsonObject jsonBookInfo;
-		jsonBookInfo = (JsonObject) parser.parse(completeBookInfo);
-		final JsonObject jsonBook = jsonBookInfo.get("book").getAsJsonObject();
-		final JsonObject jsonSeries = jsonBookInfo.get("series").getAsJsonObject();
-		final Integer year = jsonBook.get("year").getAsInt();
-		final Double rating = jsonBook.get("rating").getAsDouble();
-		final String goodreadsId = jsonBook.get("goodreadsId").getAsString();
-		final String title = jsonBook.get("title").getAsString();
-		final String image = jsonBook.get("image").getAsString();
-		final Integer authorId = jsonBook.get("authorID").getAsInt();
-		final String author = jsonBook.get("authorName").getAsString();
-		final Integer pages = jsonBook.get("pages").getAsInt();
-		final String synopsis = jsonBook.get("synopsis").getAsString();
-		final String genre = jsonBook.get("genres").getAsString();
-		final String googleId = jsonBook.get("googleId").getAsString();
+    /**
+     * Takes a {@link JsonObject} in {@link String} form and populates
+     * {@link Book} object.
+     *
+     * @param isbn
+     * @param completeBookInfo
+     * @param book
+     *            Partially populated {@link Book}.
+     * @return Fully populated {@link Book}.
+     */
+    private static Book populateBook(String isbn, String completeBookInfo, Book book) {
+        final JsonParser parser = new JsonParser();
+        JsonObject jsonBookInfo;
+        jsonBookInfo = (JsonObject) parser.parse(completeBookInfo);
+        final JsonObject jsonBook = jsonBookInfo.get("book").getAsJsonObject();
+        final JsonObject jsonSeries = jsonBookInfo.get("series").getAsJsonObject();
+        final Integer year = jsonBook.get("year").getAsInt();
+        final Double rating = jsonBook.get("rating").getAsDouble();
+        final String goodreadsId = jsonBook.get("goodreadsId").getAsString();
+        final String title = jsonBook.get("title").getAsString();
+        final String image = jsonBook.get("image").getAsString();
+        final Integer authorId = jsonBook.get("authorID").getAsInt();
+        final String author = jsonBook.get("authorName").getAsString();
+        final Integer pages = jsonBook.get("pages").getAsInt();
+        final String synopsis = jsonBook.get("synopsis").getAsString();
+        final String genre = jsonBook.get("genres").getAsString();
+        final String googleId = jsonBook.get("googleId").getAsString();
 
-		final Integer seriesNumber = jsonSeries.get("seriesNumber").getAsInt();
-		final String seriesIdString = jsonSeries.get("seriesID").getAsString();
+        final Integer seriesNumber = jsonSeries.get("seriesNumber").getAsInt();
+        final String seriesIdString = jsonSeries.get("seriesID").getAsString();
 
-		Integer seriesId = null;
-		if (seriesIdString.equals("") || seriesIdString == null) {
-			seriesId = 0;
-		}
+        Integer seriesId = null;
+        if (seriesIdString.equals("") || seriesIdString == null) {
+            seriesId = 0;
+        }
 
-		if (book.author == null || book.author.equals("")) {
-			book.setAuthor(author);
-			book.setTitle(title);
-		}
-		book.setIsbn(isbn);
-		book.setAuthorId(authorId);
-		book.setRating(rating);
-		book.setSynopsis(synopsis);
-		book.setPages(pages);
-		book.setSeriesId(seriesId);
-		book.setSeriesNumber(seriesNumber);
-		book.setYear(year);
-		book.setgenre(genre);
-		book.setImage(image);
-		book.setGoogleId(googleId);
-		book.setGoodreadsId(goodreadsId);
+        if (book.author == null || book.author.equals("")) {
+            book.setAuthor(author);
+            book.setTitle(title);
+        }
+        book.setIsbn(isbn);
+        book.setAuthorId(authorId);
+        book.setRating(rating);
+        book.setSynopsis(synopsis);
+        book.setPages(pages);
+        book.setSeriesId(seriesId);
+        book.setSeriesNumber(seriesNumber);
+        book.setYear(year);
+        book.setgenre(genre);
+        book.setImage(image);
+        book.setGoogleId(googleId);
+        book.setGoodreadsId(goodreadsId);
 
-		return book;
-	}
+        return book;
+    }
 
-	/**
-	 * Queries the database for books with needsProcessing = 1.
-	 *
-	 * @return {@link List<String>} of isbns for books that need processing.
-	 */
-	public static List<String> getIsbnsFromDBToQuery() {
-		final List<String> toReturn = new ArrayList<String>();
+    /**
+     * Queries the database for books with needsProcessing = 1.
+     *
+     * @return {@link List<String>} of isbns for books that need processing.
+     */
+    public static List<String> getIsbnsFromDBToQuery() {
+        final List<String> toReturn = new ArrayList<String>();
 
-		try {
-			final ResultSet resultSet = statement
-					.executeQuery("select isbn from princewalker.books where needsProcessing = 1");
-			while (resultSet.next()) {
-				toReturn.add(resultSet.getString("isbn"));
-			}
-		} catch (final SQLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+        try {
+            final ResultSet resultSet = statement
+                    .executeQuery("select isbn from princewalker.books where needsProcessing = 1");
+            while (resultSet.next()) {
+                toReturn.add(resultSet.getString("isbn"));
+            }
+        } catch (final SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
 
-		if (toReturn.isEmpty()) {
-			System.out.println("No books to query.");
-			System.exit(0);
-		}
+        if (toReturn.isEmpty()) {
+            System.out.println("No books to query.");
+            System.exit(0);
+        }
 
-		return toReturn;
-	}
+        return toReturn;
+    }
 
-	/**
-	 * Connects to the database and initializes {@link Connection} and
-	 * {@link Statement}.
-	 */
-	private static void connectToDB() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connect = DriverManager.getConnection(
-					"jdbc:mysql://princewalker.db.9903859.hostedresource.com?" + "user=princewalker&password=W@lker75");
+    public static boolean checkIfBookExistsInDB(String isbn) {
+        connectToDB();
 
-			statement = connect.createStatement();
-		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            final ResultSet resultSet = statement.executeQuery(
+                    "select count(*) as count from princewalker.books where isbn = " + isbn);
+            if (resultSet.getInt("count") == 1) {
+                return true;
+            }
+        } catch (final SQLException e) {
+            return false;
+        }
+        return false;
+    }
 
-	/**
-	 * Prints isbns in isbnList.
-	 *
-	 * @param isbnList
-	 */
-	private static void printISBNs(List<String> isbnList) {
-		for (final String bookIsbn : isbnList) {
-			System.out.println(bookIsbn);
-		}
-	}
+    /**
+     * Connects to the database and initializes {@link Connection} and
+     * {@link Statement}.
+     */
+    private static void connectToDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://princewalker.db.9903859.hostedresource.com?"
+                            + "user=princewalker&password=W@lker75");
+
+            statement = connect.createStatement();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prints isbns in isbnList.
+     *
+     * @param isbnList
+     */
+    private static void printISBNs(List<String> isbnList) {
+        for (final String bookIsbn : isbnList) {
+            System.out.println(bookIsbn);
+        }
+    }
 }
