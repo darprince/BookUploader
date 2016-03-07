23c746c8dc3c36f81b875d6b3892737feea4cb97
Darren Prince <darprince@hotmail.com>
2016-03-07 09:45:57 -0800

    Added method to check if book already exists on server.

--- src/main/java/com/dprince/bookUploader/BookUploader.java
+++ src/main/java/com/dprince/bookUploader/BookUploader.java
@@ -14,8 +14,24 @@ import org.apache.logging.log4j.Logger;
 public class BookUploader {
 	static Logger log = LogManager.getLogger(BookUploader.class.getName());
 
+	private static final String SERVER = "www.princewalker.com";
+	private static final int PORT = 21;
+	private static final String USER = "prin9762";
+	private static final String PASS = "W@lker75";
+
 	public static void main(String[] args) {
 		final Book book = setUpInstanceOfBook(args[0]);
+
+		boolean success = false;
+		try {
+			success = checkFileExists("books/" + book.filename);
+			if (success) {
+				log.info("Book exists.");
+				System.exit(0);
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
 
 		log.info("getIsbnFromGoogle()");
 		final List<String> potentialIsbnList = Google.getPotentialIsbnsFromGoogle(book.author, book.title);
@@ -58,16 +74,11 @@ public class BookUploader {
 	 * @return true if upload succeeds, false otherwise.
 	 */
 	public static boolean uploadFile(String filename, String fullFileName, String isbn) {
-		final String server = "www.princewalker.com";
-		final int port = 21;
-		final String user = "prin9762";
-		final String pass = "W@lker75";
-
 		log.info("uploadFile()");
 		final FTPClient ftpClient = new FTPClient();
 		try {
-			ftpClient.connect(server, port);
-			ftpClient.login(user, pass);
+			ftpClient.connect(SERVER, PORT);
+			ftpClient.login(USER, PASS);
 			ftpClient.enterLocalPassiveMode();
 
 			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
@@ -97,6 +108,32 @@ public class BookUploader {
 			} catch (final IOException ex) {
 				ex.printStackTrace();
 			}
+		}
+
+		boolean success = false;
+		try {
+			success = checkFileExists("books/" + filename);
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
+		return success;
+	}
+
+	static boolean checkFileExists(String filepath) throws IOException {
+		final FTPClient ftpClient = new FTPClient();
+		try {
+			ftpClient.connect(SERVER, PORT);
+			ftpClient.login(USER, PASS);
+			ftpClient.enterLocalPassiveMode();
+
+			final InputStream inputStream = ftpClient.retrieveFileStream(filepath);
+			final Integer returnCode = ftpClient.getReplyCode();
+			if (inputStream == null || returnCode == 550) {
+				return false;
+			}
+		} catch (final Exception e) {
+
 		}
 		return true;
 	}
